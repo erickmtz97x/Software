efc851f551874f31407cb8d3fa107d7fff3b2e87
erickmtz97x <112738081+erickmtz97x@users.noreply.github.com>
2022-09-05 23:32:23 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,26 @@
+print("################ CUESTIONARIO ################")
+
+pregunta1 = "¿Quien es el creador de Apple? = "
+
+respuesta1 = "Steve Jobs"
+
+entrada1 = input(f"pregunta 1 : {pregunta1}")
+
+aciertos1 = False
+
+if (entrada1 == respuesta1):
+	aciertos1 = True 
+
+else 
+
+	aciertos1 = False
+
+
+print("\n")
+
+
+if (aciertos1 == True):
+	print("Pregunta 1 Correcta")
+
+else 
+	print("Pregunta 1 Incorrecta")
